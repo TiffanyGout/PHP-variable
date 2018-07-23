caafd0f1596eb891d61ada8287826eb100e599c6
TiffanyGouttenoire0610 <40634369+TiffanyGouttenoire0610@users.noreply.github.com>
2018-07-23 12:25:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+//variables
+$name = "Tiffany GOUTTENOIRE";
+
+$lastname = "Tiffany";
+$firstname = "GOUTTENOIRE";
+$age = 22;
+
+echo $lastname.$firstname."a".$age;
+
+//variables km
+$km="1";
+echo "$km";
+$km="3";
+echo "$km";
+$km ="125";
+echo "$km";
+
+//variable type string
+$name="GOUTTENOIRE22";
+echo "$name";
+
+//variable type int
+$age=22;
+echo "$age";
+
+//variable type float
+$poids= 51.85;
+echo "$poids";
+
+//variable boléen
+$je_suis_une_femme = true;
+$je_suis_un_homme = false;
+
+//type int sans rien
+$age=NULL;
+echo "$age";
+
+//variable name exercice 6
+$name = "Tiffany";
+echo "Bonjour" . $name . ", comment vas-tu?";
+
+//variable lastname, firstname, age
+$lastname = "GOUTTENOIRE";
+$firstname = "Tiffany";
+$age = 22;
+echo "$lastname . $firstname . $age";
+
+//variable 3+4
+$nombre = 3 + 4;
+echo "$nombre";
+
+//variable 5*20
+$nombre = 5 * 20;
+echo "$nombre";
+
+//variable 45/5
+$nombre = 45 / 5;
+echo "$nombre";
